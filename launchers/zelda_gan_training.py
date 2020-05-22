bb32c05be6cf4629ba4130eb37f74d77d789a7d0
Hejia Zhang <hjzh578@gmail.com>
2020-05-22 09:20:46 -0700

    running gan training

--- launchers/zelda_gan_training.py
+++ launchers/zelda_gan_training.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+"""This file is for pure gan experiment."""
 
 import argparse
 import math
@@ -17,47 +17,40 @@ import json
 from utils.TrainLevelHelper import get_lvls, get_integer_lvl
 
 
-def run():
+def run(nz,
+        ngf,
+        ndf,
+        batch_size,
+        niter,
+        lrD,
+        lrG,
+        beta1,
+        cuda,
+        ngpu,
+        gpu_id,
+        path_netG,
+        path_netD,
+        clamp_lower,
+        clamp_upper,
+        D_iters,
+        n_extra_layers,
+        gan_experiment,
+        adam,
+        seed,
+        lvl_data):
     os.chdir(".")
     print(os.getcwd())
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--nz', type=int, default=32, help='size of the latent z vector')
-    parser.add_argument('--ngf', type=int, default=64)
-    parser.add_argument('--ndf', type=int, default=64)
-    parser.add_argument('--batchSize', type=int, default=50, help='input batch size')
-    parser.add_argument('--niter', type=int, default=25000, help='number of epochs to train for')
-    parser.add_argument('--lrD', type=float, default=0.00005, help='learning rate for Critic, default=0.00005')
-    parser.add_argument('--lrG', type=float, default=0.00005, help='learning rate for Generator, default=0.00005')
-    parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
-    parser.add_argument('--cuda', action='store_true', help='enables cuda')
-    parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
-    parser.add_argument('--netG', default='', help="path to netG (to continue training)")
-    parser.add_argument('--netD', default='', help="path to netD (to continue training)")
-    parser.add_argument('--clamp_lower', type=float, default=-0.01)
-    parser.add_argument('--clamp_upper', type=float, default=0.01)
-    parser.add_argument('--Diters', type=int, default=5, help='number of D iters per each G iter')
+    if not os.path.exists(gan_experiment):
+        os.system('mkdir {0}'.format(gan_experiment))
 
-    parser.add_argument('--n_extra_layers', type=int, default=0, help='Number of extra layers on gen and disc')
-    parser.add_argument('--experiment', default=None, help='Where to store samples and models')
-    parser.add_argument('--adam', action='store_true', help='Whether to use adam (default is rmsprop)')
-    parser.add_argument('--problem', type=int, default=0, help='Level examples')
-    parser.add_argument('--json', default=None, help='Json file')
-    parser.add_argument('--seed', type=int, default=999, help='random seed for reproducibility')
-    opt = parser.parse_args()
-
-    if opt.experiment is None:
-        opt.experiment = 'zelda_samples'
-    if not os.path.exists(opt.experiment):
-        os.system('mkdir {0}'.format(opt.experiment))
-
-    print("Manual Seed: ", opt.seed)
-    random.seed(opt.seed)
-    torch.manual_seed(opt.seed)
+    print("Manual Seed: ", seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
 
     cudnn.benchmark = True
 
-    if torch.cuda.is_available() and not opt.cuda:
+    if torch.cuda.is_available() and not cuda:
         print("WARNING: You have a CUDA device, so you should probably run with --cuda")
 
     map_size = 32
@@ -78,9 +71,9 @@ def run():
     X = np.array(np_lvls)
     z_dims = len(index2strJson)
 
-    num_batches = X.shape[0] / opt.batchSize
+    num_batches = X.shape[0] / batch_size
 
-    print("Batch size is " + str(opt.batchSize))
+    print("Batch size is " + str(batch_size))
 
     print("SHAPE ", X.shape)
     X_onehot = np.eye(z_dims, dtype='uint8')[X]
@@ -95,13 +88,6 @@ def run():
     # Pad part of level so its a square
     X_train[:X.shape[0], :, :X.shape[1], :X.shape[2]] = X_onehot
 
-    ngpu = int(opt.ngpu)
-    nz = int(opt.nz)
-    ngf = int(opt.ngf)
-    ndf = int(opt.ndf)
-
-    n_extra_layers = int(opt.n_extra_layers)
-
     # custom weights initialization called on netG and netD
     def weights_init(m):
         classname = m.__class__.__name__
@@ -112,58 +98,39 @@ def run():
             m.bias.data.fill_(0)
 
     netG = dcgan.DCGAN_G(map_size, nz, z_dims, ngf, ngpu, n_extra_layers)
-
     netG.apply(weights_init)
-    if opt.netG != '':  # load checkpoint if needed
-        netG.load_state_dict(torch.load(opt.netG))
-    # print(netG)
+    if path_netG != '':  # load checkpoint if needed
+        netG.load_state_dict(torch.load(path_netG))
 
     netD = dcgan.DCGAN_D(map_size, nz, z_dims, ndf, ngpu, n_extra_layers)
     netD.apply(weights_init)
+    if path_netD != '':
+        netD.load_state_dict(torch.load(path_netD))
 
-    if opt.netD != '':
-        netD.load_state_dict(torch.load(opt.netD))
-    # print(netD)
-
-    input = torch.FloatTensor(opt.batchSize, z_dims, map_size, map_size)
-    noise = torch.FloatTensor(opt.batchSize, nz, 1, 1)
-    fixed_noise = torch.FloatTensor(opt.batchSize, nz, 1, 1).normal_(0, 1)
+    input = torch.FloatTensor(batch_size, z_dims, map_size, map_size)
+    noise = torch.FloatTensor(batch_size, nz, 1, 1)
+    fixed_noise = torch.FloatTensor(batch_size, nz, 1, 1).normal_(0, 1)
     one = torch.FloatTensor([1])
     mone = one * -1
 
-    def tiles2image(tiles):
-        return plt.get_cmap('rainbow')(tiles / float(z_dims))
-
-    def combine_images(generated_images):
-        num = generated_images.shape[0]
-        width = int(math.sqrt(num))
-        height = int(math.ceil(float(num) / width))
-        shape = generated_images.shape[1:]
-        image = np.zeros((height * shape[0], width * shape[1], shape[2]), dtype=generated_images.dtype)
-        for index, img in enumerate(generated_images):
-            i = int(index / width)
-            j = index % width
-            image[i * shape[0]:(i + 1) * shape[0], j * shape[1]:(j + 1) * shape[1]] = img
-        return image
-
-    if opt.cuda:
-        netD.cuda()
-        netG.cuda()
-        input = input.cuda()
-        one, mone = one.cuda(), mone.cuda()
-        noise, fixed_noise = noise.cuda(), fixed_noise.cuda()
+    if cuda:
+        netD.cuda(gpu_id)
+        netG.cuda(gpu_id)
+        input = input.cuda(gpu_id)
+        one, mone = one.cuda(gpu_id), mone.cuda(gpu_id)
+        noise, fixed_noise = noise.cuda(gpu_id), fixed_noise.cuda(gpu_id)
 
     # setup optimizer
-    if opt.adam:
-        optimizerD = optim.Adam(netD.parameters(), lr=opt.lrD, betas=(opt.beta1, 0.999))
-        optimizerG = optim.Adam(netG.parameters(), lr=opt.lrG, betas=(opt.beta1, 0.999))
+    if adam:
+        optimizerD = optim.Adam(netD.parameters(), lr=lrD, betas=(beta1, 0.999))
+        optimizerG = optim.Adam(netG.parameters(), lr=lrG, betas=(beta1, 0.999))
         print("Using ADAM")
     else:
-        optimizerD = optim.RMSprop(netD.parameters(), lr=opt.lrD)
-        optimizerG = optim.RMSprop(netG.parameters(), lr=opt.lrG)
+        optimizerD = optim.RMSprop(netD.parameters(), lr=lrD)
+        optimizerG = optim.RMSprop(netG.parameters(), lr=lrG)
 
     gen_iterations = 0
-    for epoch in range(opt.niter):
+    for epoch in range(niter):
 
         # ! data_iter = iter(dataloader)
 
@@ -181,34 +148,26 @@ def run():
             if gen_iterations < 25 or gen_iterations % 500 == 0:
                 Diters = 100
             else:
-                Diters = opt.Diters
+                Diters = D_iters
             j = 0
             while j < Diters and i < num_batches:  # len(dataloader):
                 j += 1
 
                 # clamp parameters to a cube
                 for p in netD.parameters():
-                    p.data.clamp_(opt.clamp_lower, opt.clamp_upper)
+                    p.data.clamp_(clamp_lower, clamp_upper)
 
-                data = X_train[i * opt.batchSize:(i + 1) * opt.batchSize]
+                data = X_train[i * batch_size:(i + 1) * batch_size]
 
                 i += 1
 
                 real_cpu = torch.FloatTensor(data)
 
-                if (False):
-                    # im = data.cpu().numpy()
-                    print(data.shape)
-                    real_cpu = combine_images(tiles2image(np.argmax(data, axis=1)))
-                    print(real_cpu)
-                    plt.imsave('{0}/real_samples.png'.format(opt.experiment), real_cpu)
-                    exit()
-
                 netD.zero_grad()
                 # batch_size = num_samples #real_cpu.size(0)
 
-                if opt.cuda:
-                    real_cpu = real_cpu.cuda()
+                if cuda:
+                    real_cpu = real_cpu.cuda(gpu_id)
 
                 input.resize_as_(real_cpu).copy_(real_cpu)
                 inputv = Variable(input)
@@ -217,7 +176,7 @@ def run():
                 errD_real.backward(one)
 
                 # train with fake
-                noise.resize_(opt.batchSize, nz, 1, 1).normal_(0, 1)
+                noise.resize_(batch_size, nz, 1, 1).normal_(0, 1)
                 noisev = Variable(noise, volatile=True)  # totally freeze netG
                 fake = Variable(netG(noisev).data)
                 inputv = fake
@@ -234,7 +193,7 @@ def run():
             netG.zero_grad()
             # in case our last batch was the tail batch of the dataloader,
             # make sure we feed a full batch of noise
-            noise.resize_(opt.batchSize, nz, 1, 1).normal_(0, 1)
+            noise.resize_(batch_size, nz, 1, 1).normal_(0, 1)
             noisev = Variable(noise)
             fake = netG(noisev)
             errG = netD(fake)
@@ -243,20 +202,64 @@ def run():
             gen_iterations += 1
 
             print('[%d/%d][%d/%d][%d] Loss_D: %f Loss_G: %f Loss_D_real: %f Loss_D_fake %f'
-                  % (epoch, opt.niter, i, num_batches, gen_iterations,
+                  % (epoch, niter, i, num_batches, gen_iterations,
                      errD.data[0], errG.data[0], errD_real.data[0], errD_fake.data[0]))
 
-        if epoch % 1000 == 999 or epoch == opt.niter - 1:  # was 500
+        if epoch % 1000 == 999 or epoch == niter - 1:  # was 500
             fake = netG(Variable(fixed_noise, volatile=True))
             im = fake.data[:, :, :9, :13].cpu().numpy()
             im = np.argmax(im, axis=1)
 
-            f = open('{0}/fake_level_epoch_{1}_{2}.json'.format(opt.experiment, epoch, opt.seed), "w")
+            f = open('{0}/fake_level_epoch_{1}_{2}.json'.format(gan_experiment, epoch, seed), "w")
             f.write(json.dumps(im[0].tolist()))
             f.close()
 
-            torch.save(netG.state_dict(), '{0}/netG_epoch_{1}_{2}.pth'.format(opt.experiment, epoch, opt.seed))
+            torch.save(netG.state_dict(), '{0}/netG_epoch_{1}_{2}.pth'.format(gan_experiment, epoch, seed))
 
 
 if __name__ == '__main__':
-    run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--nz', type=int, default=32, help='size of the latent z vector')
+    parser.add_argument('--ngf', type=int, default=64)
+    parser.add_argument('--ndf', type=int, default=64)
+    parser.add_argument('--batch_size', type=int, default=50, help='input batch size')
+    parser.add_argument('--niter', type=int, default=25000, help='number of epochs to train for')
+    parser.add_argument('--lrD', type=float, default=0.00005, help='learning rate for Critic, default=0.00005')
+    parser.add_argument('--lrG', type=float, default=0.00005, help='learning rate for Generator, default=0.00005')
+    parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
+    parser.add_argument('--cuda', action='store_true', help='enables cuda', default=True)
+    parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
+    parser.add_argument('--gpu_id', type=int, default=0, help='the id of the gpu to use')
+    parser.add_argument('--path_netG', default='', help="path to netG (to continue training)")
+    parser.add_argument('--path_netD', default='', help="path to netD (to continue training)")
+    parser.add_argument('--clamp_lower', type=float, default=-0.01)
+    parser.add_argument('--clamp_upper', type=float, default=0.01)
+    parser.add_argument('--D_iters', type=int, default=5, help='number of D iters per each G iter')
+    parser.add_argument('--n_extra_layers', type=int, default=0, help='Number of extra layers on gen and disc')
+    parser.add_argument('--gan_experiment', help='Where to store samples and models')
+    parser.add_argument('--adam', action='store_true', help='Whether to use adam (default is rmsprop)')
+    parser.add_argument('--seed', type=int, default=999, help='random seed for reproducibility')
+    parser.add_argument('--lvl_data', help='Path to the human designed levels.')
+    opt = parser.parse_args()
+
+    run(opt.nz,
+        opt.ngf,
+        opt.ndf,
+        opt.batch_size,
+        opt.niter,
+        opt.lrD,
+        opt.lrG,
+        opt.beta1,
+        opt.cuda,
+        opt.ngpu,
+        opt.gpu_id,
+        opt.path_netG,
+        opt.path_netD,
+        opt.clamp_lower,
+        opt.clamp_upper,
+        opt.D_iters,
+        opt.n_extra_layers,
+        opt.gan_experiment,
+        opt.adam,
+        opt.seed,
+        opt.lvl_data)
