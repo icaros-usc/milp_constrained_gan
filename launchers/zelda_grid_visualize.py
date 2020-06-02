3478e2afe3327b1f852674ebf066450d089b1fc6
Hejia Zhang <hjzh578@gmail.com>
2020-06-01 17:19:56 -0700

    update grid visualize

--- launchers/zelda_grid_visualize.py
+++ launchers/zelda_grid_visualize.py
@@ -12,7 +12,7 @@ def run(dataroot,
         """If there is no such a folder."""
         os.makedirs(output_path, exist_ok=True)
 
-    sprites_root = os.path.join(os.path.dirname(os.path.dirname(__file__)), '../GVGAI', 'sprites', 'oryx')
+    sprites_root = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'GVGAI', 'sprites', 'oryx')
     sprites_path = {0: 'wall3.png', 1: 'floor3.png', 2: 'key3.png', 3: 'doorclosed1.png',
                     4: 'bat1.png', 5: 'bear1.png', 6: 'bee1.png', 7: 'swordman1_0.png'}
 
@@ -26,8 +26,15 @@ def run(dataroot,
 
 
 if __name__ == '__main__':
-    dataroot = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'zelda', 'gan')
-    output_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'zelda',
-                               'gan_visual')
-    run(dataroot,
-        output_path)
+    data_root = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'zelda')
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--lvl_path', help='the name of the folder that contains generated levels.')
+    parser.add_argument('--output_folder', help='the name of the folder where we output generated levels')
+    opt = parser.parse_args()
+
+    lvl_folder = os.path.join(data_root, opt.lvl_path)
+    output_folder = os.path.join(data_root, opt.output_folder)
+
+    run(lvl_folder,
+        output_folder)
