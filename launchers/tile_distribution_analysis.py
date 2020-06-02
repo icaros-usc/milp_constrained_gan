5d3d5c0e88589efe6dda306e7e01e02771b81677
Hejia Zhang <hjzh578@gmail.com>
2020-06-01 17:04:39 -0700

    refactor tile distribution analysis code

--- launchers/tile_distribution_analysis.py
+++ launchers/tile_distribution_analysis.py
@@ -6,7 +6,7 @@ import os
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 
-from launchers.hamming_distance_analysis import get_valid_lvls
+from launchers.hamming_distance_analysis import get_valid_lvls, get_valid_and_unique_lvls
 
 
 def compute_tile_number(lvl, tile_type):
@@ -41,58 +41,58 @@ def do_stats(valid_lvls, tile_type):
     return langs, counts
 
 
-def run():
-    gan_generated_root = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'zelda', 'fake')
-    milp_gan_two_stage_root = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'zelda',
-                                           'fake_milp_gan_obj')
-    human_generated_root = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'zelda', 'Human_Json')
+def run(data_root,
+        human_root,
+        to_stats):
+    tile_types = [[1], [0], [4, 5, 6]]
+    plot_titles = ['Empty Distribution', 'Walls Distribution', 'Enemy Distribution']
+    plot_outputs = ['em_dist.pdf', 'walls_dist.pdf', 'enemy_dist.pdf']
+    for tile_type, plot_title, plot_output in zip(tile_types, plot_titles, plot_outputs):
+        human_data_path = os.path.join(data_root, human_root)
+        human_valid_lvls = get_valid_lvls(human_data_path)
+        human_langs, human_counts = do_stats(human_valid_lvls, tile_type)
 
-    # first get all valid levels generated
-    all_valid_gan_lvls = get_valid_lvls(gan_generated_root)
-    all_two_stage_lvls = get_valid_lvls(milp_gan_two_stage_root)
-    all_human_lvls = get_valid_lvls(human_generated_root)
+        left_tick = 1000
+        right_tick = 0
+        for key in to_stats:
+            cur_data_path = os.path.join(data_root, to_stats[key]['path'])
+            cur_valid_lvls = get_valid_lvls(cur_data_path)
+            cur_langs, cur_counts = do_stats(cur_valid_lvls, tile_type)
+            if cur_langs[-1] > right_tick:
+                right_tick = cur_langs[-1]
+            if cur_langs[0] < left_tick:
+                left_tick = cur_langs[0]
+            to_stats[key]['langs'] = cur_langs
+            to_stats[key]['counts'] = cur_counts
 
-    plot_type = 2  # 1: empty space; 2: wall; 3: enemy
-
-    if plot_type == 1:
-        tile_type = [1]
-        title = 'Empty Distribution'
-        output = 'em_dist.pdf'
-    elif plot_type == 2:
-        tile_type = [0]
-        title = 'Walls Distribution'
-        output = 'walls_dist.pdf'
-    elif plot_type == 3:
-        tile_type = [4, 5, 6]
-        title = 'Enemy Distribution'
-        output = 'enemy_dist.pdf'
-    else:
-        raise NotImplementedError('The input type {} is not valid'.format(plot_type))
-
-    gan_langs, gan_counts = do_stats(all_valid_gan_lvls, tile_type)
-    two_stage_langs, two_stage_counts = do_stats(all_two_stage_lvls, tile_type)
-    human_langs, human_counts = do_stats(all_human_lvls, tile_type)
-
-    left_tick = min(gan_langs[0], two_stage_langs[0], human_langs[0])
-    right_tick = max(gan_langs[-1], two_stage_langs[-1], human_langs[-1])
-
-    fig, ax = plt.subplots(figsize=(6, 4.5))
-    # ax = fig.add_axes([0, 0, 1, 1])
-    ax.bar(gan_langs, gan_counts, color='#ff9f4a', edgecolor='#ff871d', alpha=0.7, width=1, label='GAN')
-    ax.bar(human_langs, human_counts, color='#5699c5', edgecolor='#2d7fb8', alpha=0.7, width=1, label='Human')
-    ax.bar(two_stage_langs, two_stage_counts, color='#60b761', edgecolor='#3aa539', alpha=0.7, width=1,
-           label='GAN+MILP (Two-stage)')
-    ax.set_ylabel('Count')
-    ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
-    ax.set_xlabel(title)
-    xticks = list(range(left_tick, right_tick, ((right_tick - left_tick) // 6)))[1:-1]
-    if plot_type == 3:
-        xticks = [0, 2, 4, 6, 8]
-    ax.set_xticks(xticks)
-    ax.legend()
-    fig.tight_layout()
-    plt.savefig(output)
+        # now visualize
+        fig, ax = plt.subplots(figsize=(6, 4.5))
+        # first visualize human data
+        ax.bar(human_langs, human_counts, color='#5699c5', edgecolor='#2d7fb8', alpha=0.7, width=1, label='Human')
+        # then visualize each to plot
+        for key in to_stats:
+            ax.bar(to_stats[key]['langs'], to_stats[key]['counts'],
+                   color=to_stats[key]['color'], edgecolor=to_stats[key]['edgecolor'],
+                   alpha=0.7, width=1, label=key)
+        ax.set_ylabel('Count')
+        ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+        ax.set_xlabel(plot_title)
+        xticks = list(range(left_tick, right_tick, ((right_tick - left_tick) // 6)))[1:-1]
+        if 4 in tile_type:  # plotting enemies
+            xticks = [0, 2, 4, 6, 8]
+        ax.set_xticks(xticks)
+        ax.legend()
+        fig.tight_layout()
+        plt.savefig(plot_output)
 
 
 if __name__ == '__main__':
-    run()
+    data_root = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'zelda')
+    human_root = 'Human_Json'
+    to_stats = {'GAN': {'path': 'zelda_better_gan_no_fix', 'color': '#ff9f4a', 'edgecolor': '#ff871d'},
+                'GAN + MILP (Two-Stage)': {'path': 'zelda_better_gan', 'color': '#60b761', 'edgecolor': '#3aa539'},
+                'GAN + MILP (End-to-End)': {'path': 'zelda_better_end2end_3299', 'color': '#d5a49b', 'edgecolor': '#d8a99f'}}
+
+    run(data_root,
+        human_root,
+        to_stats)
